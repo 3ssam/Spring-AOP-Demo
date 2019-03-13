8bad567d834bac1fa913e09db599d4c983152ebd
Mohamed Essam <ana3ssam2017@gmail.com>
2019-03-13 17:57:20 +0200

    #4

--- src/spring/aop/Aspect/LoggingAspect.java
+++ src/spring/aop/Aspect/LoggingAspect.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 @Component
 public class LoggingAspect {
 
-	@Before("execution(public void spring.aop.DAOS.AccountDAO.addAccount())")
+	@Before("execution(public void add*())")
 	public void beforeAddAccountAdvice() {
 		
 		System.out.println("Before Executions ==========> see again");

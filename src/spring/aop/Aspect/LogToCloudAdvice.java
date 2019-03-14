d24289186b2525e3470f97393101519a13243c43
Mohamed Essam <ana3ssam2017@gmail.com>
2019-03-14 13:31:40 +0200

    #13

--- src/spring/aop/Aspect/LogToCloudAdvice.java
+++ src/spring/aop/Aspect/LogToCloudAdvice.java
@@ -2,7 +2,6 @@ package spring.aop.Aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
--- src/spring/aop/Aspect/LoggingAspect.java
+++ src/spring/aop/Aspect/LoggingAspect.java
@@ -1,8 +1,9 @@
 package spring.aop.Aspect;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
@@ -12,8 +13,10 @@ import org.springframework.stereotype.Component;
 public class LoggingAspect {
 	
 	@Before("spring.aop.Aspect.HelperMethodsOfAspect.AopPackageNoSetterOrGetter()")
-	public void beforeAddAccountAdvice() {
+	public void beforeAddAccountAdvice(JoinPoint joinPoint) {
 		System.out.println("\nBefore Executions ==========> see again");
+		MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+		System.out.println("Method = "+signature);
 	}
 	
 }

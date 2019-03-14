088bf840cd3ffae78f102152f43a0e4f00856df8
Mohamed Essam <ana3ssam2017@gmail.com>
2019-03-14 12:12:06 +0200

    #10

--- src/spring/aop/Aspect/LoggingAspect.java
+++ src/spring/aop/Aspect/LoggingAspect.java
@@ -2,15 +2,26 @@ package spring.aop.Aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
+	
+	@Pointcut("execution(* spring.aop.*.*.*(..))")
+	public void aoppackage() {}
 
-	@Before("execution(* spring.aop.*.*.*(..))")
+	@Before("aoppackage()")
 	public void beforeAddAccountAdvice() {
 		
-		System.out.println("Before Executions ==========> see again");
+		System.out.println("\nBefore Executions ==========> see again");
 	}
+	
+	@Before("aoppackage()")
+	public void APiValidtion() {
+		
+		System.out.println("\nValidation Before Executions ==========> The Validation Is True");
+	}
+
 }
